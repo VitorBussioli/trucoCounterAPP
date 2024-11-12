ee867c22a7f67a4bd9fff9119111bda77c160bd6
VitorBussioli <145715526+VitorBussioli@users.noreply.github.com>
2024-11-12 18:20:07 -0300

    Add files via upload

---
+++ mobile1/windows/flutter/ephemeral/generated_config.cmake
@@ -0,0 +1,23 @@
+# Generated code do not commit.
+file(TO_CMAKE_PATH "C:\\Users\\vitor\\facul\\flutter" FLUTTER_ROOT)
+file(TO_CMAKE_PATH "C:\\Users\\vitor\\facul\\devMovel\\teste1\\mobile" PROJECT_DIR)
+
+set(FLUTTER_VERSION "1.0.0+1" PARENT_SCOPE)
+set(FLUTTER_VERSION_MAJOR 1 PARENT_SCOPE)
+set(FLUTTER_VERSION_MINOR 0 PARENT_SCOPE)
+set(FLUTTER_VERSION_PATCH 0 PARENT_SCOPE)
+set(FLUTTER_VERSION_BUILD 1 PARENT_SCOPE)
+
+# Environment variables to pass to tool_backend.sh
+list(APPEND FLUTTER_TOOL_ENVIRONMENT
+  "FLUTTER_ROOT=C:\\Users\\vitor\\facul\\flutter"
+  "PROJECT_DIR=C:\\Users\\vitor\\facul\\devMovel\\teste1\\mobile"
+  "FLUTTER_ROOT=C:\\Users\\vitor\\facul\\flutter"
+  "FLUTTER_EPHEMERAL_DIR=C:\\Users\\vitor\\facul\\devMovel\\teste1\\mobile\\windows\\flutter\\ephemeral"
+  "PROJECT_DIR=C:\\Users\\vitor\\facul\\devMovel\\teste1\\mobile"
+  "FLUTTER_TARGET=C:\\Users\\vitor\\facul\\devMovel\\teste1\\mobile\\test\\mocks.dart"
+  "DART_OBFUSCATION=false"
+  "TRACK_WIDGET_CREATION=true"
+  "TREE_SHAKE_ICONS=false"
+  "PACKAGE_CONFIG=C:\\Users\\vitor\\facul\\devMovel\\teste1\\mobile\\.dart_tool\\package_config.json"
+)
